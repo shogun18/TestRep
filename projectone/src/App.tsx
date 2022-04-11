09ec303dd295341b4db15ce60476be81f191b03f
Ayoub Sabiri <ayoub.sabiri@mminformatica.it>
2022-04-11 20:14:57 +0200

    replace random text

--- projectone/src/App.tsx
+++ projectone/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <span>
-          <span>Learn </span>
+          <span>RandomStuff</span>
           <a
             className="App-link"
             href="https://reactjs.org/"
